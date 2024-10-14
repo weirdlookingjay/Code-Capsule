3ea48f7af0bd6b1ea2b99684eafdc71d6f28b3e7
Joe Clark <weirdlookingjay@live.com>
2024-10-14 12:55:23 -0400

    Added Supabase Authform

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+
+import { AuthDialog } from "@/components/auth-dialog";
+import { AuthViewType } from "@/lib/auth";
+import { supabase } from "@/lib/supabase";
+import { useState } from "react";
+
 export default function Home() {
+  const [isAuthDialogOpen, setIsAuthDialogOpen] = useState(true);
+  const [authView, setAuthView] = useState<AuthViewType>("sign_in")
   return (
     <main className="flex min-h-screen max-h-screen">
-      <h1>Home Page</h1>
+      {supabase && (
+        <AuthDialog open={isAuthDialogOpen} setOpen={setIsAuthDialogOpen} view={authView} supabase={supabase} />
+      )}
+      <div className="grid -wfull md:grid-cols-2">
+        <div></div>
+      </div>
     </main>
   );
 }
---
+++ lib/auth.ts
@@ -0,0 +1,7 @@
+export type AuthViewType =
+  | "sign_in"
+  | "sign_up"
+  | "magic_link"
+  | "forgotten_password"
+  | "update_password"
+  | "verify_otp";
